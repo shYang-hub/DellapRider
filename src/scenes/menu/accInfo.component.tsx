669559928ef4b5675e14f0c3aec051f6b2220408
shYang-hub <sh_yang14@naver.com>
2020-07-03 18:04:16 +0900

    12

--- src/scenes/menu/accInfo.component.tsx
+++ src/scenes/menu/accInfo.component.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Divider, Layout, Text } from '@ui-kitten/components';
+import { StyleSheet, ScrollView } from 'react-native';
+import { Divider, Layout, Text, Button } from '@ui-kitten/components';
 import { AccInfoScreenProps } from '../../navigation/home.navigator';
 import { Toolbar } from '../../components/toolbar.component';
 import { SafeAreaLayout, SafeAreaLayoutElement, SaveAreaInset } from '../../components/safe-area-layout.component';
-import ContentView from './layout';
+
+import { ProfileAvatar } from './extra/profile-avatar.component';
+import { ProfileSetting } from './extra/profile-setting.component';
+import { Profile } from './extra/data';
+
+const profile: Profile = Profile.jenniferGreen();
 
 export const AccInfoScreen = (props: AccInfoScreenProps): SafeAreaLayoutElement => (
   <SafeAreaLayout
@@ -15,7 +20,66 @@ export const AccInfoScreen = (props: AccInfoScreenProps): SafeAreaLayoutElement 
       onBackPress={props.navigation.goBack}
     />
     <Divider/>
-    <ContentView navigation={props}/>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}>
+      <ProfileAvatar
+        style={styles.photo}
+        source={profile.photo}
+        /* editButton={renderPhotoButton}*/
+      />
+      <Text
+        style={[styles.description, styles.section]}
+        appearance='hint'>
+        {profile.description}
+      </Text>
+      <ProfileSetting
+        style={[styles.setting, styles.section]}
+        hint='First Name'
+        value={profile.firstName}
+      />
+      <ProfileSetting
+        style={styles.setting}
+        hint='Last Name'
+        value={profile.lastName}
+      />
+      <ProfileSetting
+        style={styles.setting}
+        hint='Gender'
+        value={profile.gender}
+      />
+      <ProfileSetting
+        style={styles.setting}
+        hint='Age'
+        value={`${profile.age}`}
+      />
+      <ProfileSetting
+        style={styles.setting}
+        hint='Weight'
+        value={`${profile.weight} kg`}
+      />
+      <ProfileSetting
+        style={styles.setting}
+        hint='Height'
+        value={`${profile.height} cm`}
+      />
+      <ProfileSetting
+        style={[styles.setting, styles.section]}
+        hint='Email'
+        value={profile.email}
+      />
+      <ProfileSetting
+        style={styles.setting}
+        hint='Phone Number'
+        value={profile.phoneNumber}
+      />
+      <Button
+        style={styles.doneButton}
+        /*onPress={onDoneButtonPress}*/
+        >
+        DONE
+      </Button>
+    </ScrollView>
   </SafeAreaLayout>
 );
 
@@ -27,5 +91,39 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  container: {
+    // backgroundColor: 'background-basic-color-2',
+  },
+  contentContainer: {
+    paddingVertical: 24,
+    // backgroundColor: 'background-basic-color-2',
+  },
+  photo: {
+    alignSelf: 'center',
+    width: 320,
+    height: 320,
+    borderRadius: 16,
+  },
+  photoButton: {
+    right: 16,
+    top: 32,
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+  },
+  description: {
+    padding: 24,
+    // backgroundColor: 'background-basic-color-1',
+  },
+  setting: {
+    padding: 16,
+  },
+  section: {
+    marginTop: 24,
+  },
+  doneButton: {
+    marginHorizontal: 24,
+    marginTop: 24,
   },
 });
